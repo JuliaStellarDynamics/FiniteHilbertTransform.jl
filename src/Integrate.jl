c7ba46c3b972f60988840955cd4009dcffd24d96
Mathieu Roule <mathieu.roule@polytechnique.edu>
2022-05-24 17:42:41 +0200

    generic compute_tabG function

--- src/Integrate.jl
+++ src/Integrate.jl
@@ -1,4 +1,25 @@
+"""
+    compute_tabG(uNodes, Gfun)
 
+Compute the G function values on the u nodes.
+"""
+function compute_tabG(uNodes::Vector{Float64},
+                        Gfun::Function)
+    
+    K_u = size(uNodes,1)        # Nodes number
+    tabG = zeros(Float64,K_u)   # Values array
+
+    Threads.@threads for i=1:K_u # Loop over the nodes
+
+        # Current node position
+        u_i = uNodes[i] 
+
+        # Compute the value of G[u_i]
+        tabG[i] = Gfun(u_i)
+    end
+
+    return tabG
+end
 
 
 function compute_tabG(uNodes::Vector{Float64},
