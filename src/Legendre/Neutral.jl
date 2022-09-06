ccfef9027de571929bd6f9427d237fe02181c15e
michael-petersen <petersen.michael.s@gmail.com>
2022-09-06 11:38:12 +0200

    Fix seed values for special neutral mode case at (0,0).

--- src/Legendre/Neutral.jl
+++ src/Legendre/Neutral.jl
@@ -1,52 +1,75 @@
-##################################################
-# Function to fill in all the Legendre arrays
-# for an NEUTRAL mode, i.e. Im[w] = 0.0
-##################################################
+
+
+"""
+Function to fill in all the Legendre arrays for an NEUTRAL mode, i.e. Im[w] = 0.0
+"""
 function tabLeg!_NEUTRAL(omg::Complex{Float64},
                          struct_tabLeg::structLegendreFHTtype)
-    ##########
-    tabDLeg = struct_tabLeg.tabDLeg # Name of the container for D_k(w)
-    ##########
-    romg = real(omg) # Keeping only the real part of omg
-    ##########
-    # Computing Q_k(w)
-    #####
-    tabQLeg = struct_tabLeg.tabQLeg # Name of the container for Q_k(w)
-    ######
-    # Initial values of Q_k(w)
+
     # The integral is computed in the Cauchy sense, hence the absolute values.
-    val_0_Q = log(abs(1.0-romg)) - log(abs(-1.0-romg)) + 0.0*im # Initial value for k = 0. ATTENTION, we make sure that the quantity is seen as complex. ATTENTION, not to forget the `abs'.
-    val_1_Q = 2.0 + romg*val_0_Q                       + 0.0*im # Initial value for k = 1. ATTENTION, we make sure that the quantity is seen as complex.
-    #####
-    tabQLeg!(omg,val_0_Q,val_1_Q,tabQLeg) # Computing the tabQLeg
-    ##########
-    # Computing P_k(w) if needed
-    #####
-    val_H = Heaviside(romg) # Computing the value of H[-1,1,Re[w]]. ATTENTION, the argument must be real.
-    #####
-    if (val_H == 0.0) # We are outside of the interval [-1,1]: no need to compute P_k(w)
-        ##########
-        # Computing D_k(w)
-        ##########
-        for k=0:(struct_tabLeg.Ku-1) # Loop over the Legendre indices
-            tabDLeg[k+1] = tabQLeg[k+1] # Filling in tabDLeg. ATTENTION, to the shift of the arrays.
+
+    # simplify naming convention for the container for D_k(w)
+    tabDLeg = struct_tabLeg.tabDLeg
+    tabQLeg = struct_tabLeg.tabQLeg
+    tabPLeg = struct_tabLeg.tabPLeg
+
+    # keep only the real part of omg
+    romg = real(omg)
+
+    # initial values of Q_k(w):
+
+    if romg == 1.0
+        # unique case, requires special seed values
+        val_0_Q = 0.0 + 0.0im
+        val_1_Q = 2.0 + 0.0im
+
+    else
+        # initial value for k = 0. ATTENTION, not to forget the `abs'.
+        val_0_Q = log(abs(1.0-romg)) - log(abs(-1.0-romg)) + 0.0*im
+
+        # initial value for k = 1.
+        val_1_Q = 2.0 + romg*val_0_Q                       + 0.0*im
+    end
+
+    # now the tabQLeg
+    tabQLeg!(omg,val_0_Q,val_1_Q,tabQLeg)
+
+    # compute P_k(w) if needed:
+    # Computing the value of H[-1,1,Re[w]]. ATTENTION, the argument must be real.
+    val_H = Heaviside(romg)
+
+    # We are outside of the interval [-1,1]: no need to compute P_k(w)
+    if (val_H == 0.0)
+
+        # compute D_k(w):
+
+        # Loop over the Legendre indices
+        for k=1:(struct_tabLeg.Ku)
+
+            # Filling in tabDLeg
+            tabDLeg[k] = tabQLeg[k]
         end
-        #####
-    else # We are within the interval [-1,1]: we need to compute P_k(w)
-        #####
-        tabPLeg = struct_tabLeg.tabPLeg # Name of the container for P_k(w)
-        #####
-        # Initial values of P_k(w)
-        val_0_P = 1.0  + 0.0*im # Initial value for k = 0. ATTENTION, we make sure that the quantity is seen as complex.
-        val_1_P = romg + 0.0*im # Initial value for k = 1. ATTENTION, we make sure that the quantity is seen as complex.
-        #####
+
+    # We are within the interval [-1,1]: we need to compute P_k(w)
+    else
+
+        # Initial values of P_k(w):
+        # Initial value for k = 0.
+        val_0_P = 1.0  + 0.0*im
+
+        # Initial value for k = 1.
+        val_1_P = romg + 0.0*im
+
         tabPLeg!(omg,val_0_P,val_1_P,struct_tabLeg.Ku,tabPLeg) # Computing the tabPLeg
-        ##########
-        # Computing D_k(w)
-        #####
-        for k=0:(struct_tabLeg.Ku-1) # Loop over the Legendre indices
-            tabDLeg[k+1] = tabQLeg[k+1] + im*pi*val_H*tabPLeg[k+1] # Filling in tabDLeg. ATTENTION, to the shift of the arrays
+
+        # Computing D_k(w):
+
+        # Loop over the Legendre indices
+        for k=1:(struct_tabLeg.Ku)
+
+            # Filling in tabDLeg
+            tabDLeg[k] = tabQLeg[k] + im*pi*val_H*tabPLeg[k]
         end
-        #####
+
     end
 end
